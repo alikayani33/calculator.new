92f2d5288ae7f2ee4670f3e8fd3af567326bf35b
alikayani33 <a.kspyproduction@gmail.com>
2025-04-30 08:38:53 +0200

    addin file

---
+++ calculator.js
@@ -0,0 +1,7 @@
+function calculateSimpleInterest(principal, rate, time) {
+    return (principal * rate * time) / 100;
+  }
+  
+  // Example usage:
+  console.log("Simple Interest:", calculateSimpleInterest(1000, 5, 2));
+  
